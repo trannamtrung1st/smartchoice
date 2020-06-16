631385f3e5d8d7fe7df02bf85b4f91fd177296f6
trannamtrung1st <trannamtrung1st@gmail.com>
2020-06-16 22:20:04 +0700

    add data

---
+++ SmartChoice.Analysis/SmartChoice.Analysis.JobRecommender/Recommender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.ML;
+using Microsoft.ML.Trainers;
+
+namespace SmartChoice.Analysis.JobRecommender
+{
+    public class Recommender
+    {
+    }
+}
