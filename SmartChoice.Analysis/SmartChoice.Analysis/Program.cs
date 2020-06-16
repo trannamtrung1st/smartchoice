61385fa320a33daafd2d4bc026687abc2b699e5f
trannamtrung1st <trannamtrung1st@gmail.com>
2020-06-16 13:29:53 +0700

    add analysis prj

---
+++ SmartChoice.Analysis/SmartChoice.Analysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartChoice.Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
